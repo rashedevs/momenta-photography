9186b70f594d9d38845e7d9d3da2f2b8080a6651
rashedevs <rasheduap2015@gmail.com>
2022-04-17 04:11:34 +0600

    responsive header added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <header>
-            <h2>This is Header</h2>
-        </header>
+        <Navbar collapseOnSelect sticky='top' expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="/" style={{ mouse: "pointer" }}>Momenta</Navbar.Brand>
+                {/* <Navbar.Brand as={Link} to="/">
+                <img height={35} src={logo} alt="" />
+            </Navbar.Brand> */}
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                    </Nav>
+                    <Nav>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                        {/* {
+                        user ?
+                            <button className='btn btn-link text-white text-decoration-none' onClick={() => signOut(auth)}>Sign Out</button>
+                            :
+                            <Nav.Link eventKey={2} as={Link} to="/login">
+                                Login
+                            </Nav.Link>} */}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
