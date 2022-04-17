5539d45a0ac69c496eb0da0dea78adc0378f1345
rashedevs <rasheduap2015@gmail.com>
2022-04-17 23:45:16 +0600

    custom link implemented on header

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,6 +4,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import camera from '../../../images/logo/camera.jpg';
+import CustomLink from '../../CustomLink/CustomLink';
 import './Header.css';
 
 const Header = () => {
@@ -13,24 +14,21 @@ const Header = () => {
             <Container>
                 <Navbar.Brand as={Link} to="/" style={{ mouse: "pointer", fontSize: "22px" }}>
                     <span><img style={{ width: "32px" }} src={camera} alt="" /></span> Momenta Photography</Navbar.Brand>
-                {/* <Navbar.Brand as={Link} to="/">
-                <img height={35} src={logo} alt="" />
-            </Navbar.Brand> */}
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                     </Nav>
                     <Nav className='all-links'>
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
-                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <CustomLink to="/">Home</CustomLink>
+                        <CustomLink to="/blogs">Blogs</CustomLink>
+                        <CustomLink to="/about">About</CustomLink>
                         {
                             user ?
                                 <button className='logout btn btn-link text-white text-decoration-none' onClick={() => signOut(auth)}>Sign Out</button>
                                 :
-                                <Nav.Link eventKey={2} as={Link} to="/login">
+                                <CustomLink eventkey={2} as={Link} to="/login">
                                     Login
-                                </Nav.Link>}
+                                </CustomLink>}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
