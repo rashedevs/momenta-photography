a0daad3040521d06c95a5a2a8390ed17139e4376
rashedevs <rasheduap2015@gmail.com>
2022-04-18 15:10:32 +0600

    about component updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,11 +3,12 @@ import './About.css';
 
 const About = () => {
     return (
-        <div className='about-box' style={{ minHeight: "100vh" }}>
+        <div className='about-box' style={{ minHeight: "90vh" }}>
             <div className='about-me'>
-                <h3>Rased Uz Zaman</h3>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur ea rerum reiciendis quidem unde quibusdam, pariatur autem culpa officiis fugiat rem ab hic repellat quia exercitationem quam temporibus necessitatibus nemo? Voluptates autem blanditiis temporibus magnam nemo necessitatibus voluptatum nostrum! Dolor voluptate nihil temporibus maxime deserunt ab tempore, libero corporis atque ex expedita illo perferendis nesciunt totam quo animi, eaque sit fugit consectetur repellat aliquam vitae delectus. Iusto repellendus odio officiis ullam quia eligendi veritatis laborum facilis molestias? Repudiandae, fugit harum.
+                <h3>Rashed Uz Zaman</h3>
+                <div className='w-50 mx-auto'><hr /></div>
+                <p className='px-5'>
+                    My goal is to be a fullstack software developer. I strictly follow my routine to achieve my goal. I have started with javascript and nowadays learning front-end technologies. After that, I have a proper plan for Node.js and its back-end related tools.
                 </p>
             </div>
         </div>
