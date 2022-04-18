437ca87c5e20a8356eb5c68499321592fe86f9fe
rashedevs <rasheduap2015@gmail.com>
2022-04-18 13:29:09 +0600

    checkout form modified

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -10,21 +10,26 @@ const Checkout = () => {
         event.preventDefault()
         const name = event.target.name.value
         const email = event.target.email.value
+        const address = event.target.address.value
         const phone = event.target.phone.value
-        if (name && email && phone) {
+        if (name && email && address && phone) {
             toast('Thank you for the booking')
         }
     }
     return (
         <>
-            <h3 className='text-primary mt-5'>Your selected service ID: {serviceId}</h3>
+            <h3 className='text-primary mt-5'>Your selected service: {serviceId}</h3>
             <div className='container' style={{ minHeight: "60vh" }}>
                 <Form onSubmit={handleCheckout} className='w-50 mx-auto mt-5'>
                     <Form.Group className="mb-5" controlId="formBasicEmail">
-                        <Form.Control name='name' type="text" placeholder="Enter your name" required />
+                        <Form.Control name='name' type="text" placeholder="Your name" required />
                     </Form.Group>
                     <Form.Group className="mb-5" controlId="formBasicEmail">
                         <Form.Control name='email' type="email" placeholder="Enter email" required />
+                    </Form.Group>
+
+                    <Form.Group className="mb-5" controlId="formBasicEmail">
+                        <Form.Control name='address' type="text" placeholder="Address" required />
                     </Form.Group>
 
                     <Form.Group className="mb-4" controlId="formBasicPassword">
