de52240d0179cdec8fbf931ffbd0bdf78617bfca
rashedevs <rasheduap2015@gmail.com>
2022-04-18 15:32:53 +0600

    footer updated

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Footer.css';
+import foot from '../../../images/foot.png';
 
 const Footer = () => {
     return (
         <footer className='container-fluid footer-box'>
+            <div>
+                <img style={{ height: "100px" }} src={foot} alt="" />
+            </div>
             <div className="footer-items">
                 <h6>Momenta Photography</h6>
                 <p><small>Copyright &copy; {new Date().getFullYear()}</small></p>
