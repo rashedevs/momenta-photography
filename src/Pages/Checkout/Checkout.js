52fb03c8803f5423e1db77b4a3dc3a46f315f5ff
rashedevs <rasheduap2015@gmail.com>
2022-04-18 06:15:24 +0600

    route set on about me button

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -9,26 +9,20 @@ const Checkout = () => {
     const handleCheckout = (event) => {
         event.preventDefault()
         const name = event.target.name.value
-        const address = event.target.address.value
         const email = event.target.email.value
         const phone = event.target.phone.value
-        if (name && address && email && phone) {
-            toast('Thank you for the booking.')
+        if (name && email && phone) {
+            toast('Thank you for the booking')
         }
     }
     return (
         <>
             <h3 className='text-primary mt-5'>Your selected service ID: {serviceId}</h3>
-            <div className='container' style={{ minHeight: "70vh" }}>
+            <div className='container' style={{ minHeight: "60vh" }}>
                 <Form onSubmit={handleCheckout} className='w-50 mx-auto mt-5'>
                     <Form.Group className="mb-5" controlId="formBasicEmail">
                         <Form.Control name='name' type="text" placeholder="Enter your name" required />
                     </Form.Group>
-
-                    <Form.Group className="mb-5" controlId="formBasicEmail">
-                        <Form.Control name='address' type="text" placeholder="Enter your address" required />
-                    </Form.Group>
-
                     <Form.Group className="mb-5" controlId="formBasicEmail">
                         <Form.Control name='email' type="email" placeholder="Enter email" required />
                     </Form.Group>
--- src/Pages/Home/Head/Head.js
+++ src/Pages/Home/Head/Head.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Head.css';
 
 const Head = () => {
+    const navigate = useNavigate()
     return (
         <div className='head mb-5'>
             <div className='head-box'>
@@ -14,7 +15,7 @@ const Head = () => {
                 <div className='detail'>
                     <h4 className='text-warning'>Photographer by born.</h4>
                     <h3 className=''>My clicks creates history, be a part of that.</h3>
-                    <button as={Link} a="/checkout" className='btn-danger border-none rounded px-3 py-1 fw-bold'>View Plans</button>
+                    <button onClick={() => navigate('/about')} className='btn-danger border-none rounded px-3 py-1 fw-bold mt-2'>About Me</button>
                 </div>
             </div>
 
